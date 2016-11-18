e13d252076bba9a031d30c871447435e228860ce
Dale Hamel <dale.hamel@shopify.com>
2016-11-18 14:58:26 -0500

    Add support for branched data bags

---
+++ lib/chef_git/branched_data_bag.rb
@@ -0,0 +1,40 @@
+require 'chef/node'
+require 'mixlib/shellout'
+
+module ChefGit::BranchedDataBag
+  extend self
+
+  def bag_name(data_bag)
+    if chef_git_environment == 'master'
+      return data_bag
+    elsif data_bag_changed?(data_bag)
+      return "#{chef_git_environment}__#{data_bag}"
+    else
+      return data_bag
+    end
+  end
+
+  private
+
+  def data_bag_changed?(data_bag)
+    Dir.chdir(ChefGit::REPO_PATH) do
+      git_diff = Mixlib::ShellOut.new("git diff origin/master data_bags/#{data_bag}")
+      git_diff.run_command
+      unchanged = !git_diff.error? && git_diff.stdout.empty? && git_diff.stderr.empty?
+      return !unchanged
+    end
+  end
+
+  def chef_git_environment
+    @branch ||= begin
+      # If the branch is specified in client.rb or -E flag, use that
+      if Chef::Config.environment
+        Chef::Config.environment
+      # Otherwise, look up the node
+      else
+        node = Chef::Node.load(Chef::Config[:node_name])
+        node.chef_environment
+      end
+    end
+  end
+end
--- lib/chef_git/data_bag_item.rb
+++ lib/chef_git/data_bag_item.rb
@@ -1,13 +1,20 @@
+require 'chef_git/branched_data_bag'
 require 'chef/encrypted_data_bag_item'
-
-# We are forcing chef solo mode so that we use the plain data bag items from git
-# If the items are encrypted, we use the chef server
-# https://github.com/chef/chef/blob/db57131ad383076391b9df32d5e9989cfb312d58/lib/chef/data_bag_item.rb#L149-L156
 
 class ChefGit::DataBagItem < Chef::DataBagItem
 
   def self.load(data_bag, name)
-    Chef::Config[:solo_legacy_mode] = chef_git_use_solo?(data_bag, name)
+    # We are forcing chef solo mode so that we use the plain data bag items from git
+    # If the items are encrypted, we use the chef server
+    # https://github.com/chef/chef/blob/db57131ad383076391b9df32d5e9989cfb312d58/lib/chef/data_bag_item.rb#L149-L156
+
+    use_solo = chef_git_use_solo?(data_bag, name)
+    unless use_solo
+      # If we aren't using solo, we'll see if we need to prefix our data bag with a branch
+      data_bag = ChefGit::BranchedDataBag.bag_name(data_bag)
+    end
+
+    Chef::Config[:solo_legacy_mode] = use_solo
     result = super
     Chef::Config[:solo_legacy_mode] = false
     return result
