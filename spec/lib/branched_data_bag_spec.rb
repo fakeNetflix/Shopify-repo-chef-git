8e381335f7557421db6528b61dee3c48c253075d
Andrew Brown <andrew.brown@shopify.com>
2016-12-05 12:04:01 -0500

    Add tests

--- spec/lib/branched_data_bag_spec.rb
+++ spec/lib/branched_data_bag_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe ChefGit::BranchedDataBag do
     before do
       allow_any_instance_of(ChefGit::BranchedDataBag).to receive(:chef_git_environment).and_return('notmaster')
       allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return("0\t3")
+      allow(Chef::DataBag).to receive(:load).and_return({'foo' => 'bar'})
       Dir.stub(:chdir).and_yield
     end
 
@@ -50,5 +51,33 @@ RSpec.describe ChefGit::BranchedDataBag do
       expect(ChefGit::BranchedDataBag.bag_name('foo')).to eq('foo')
     end
 
+  end
+
+  context 'branched data bag not on chef server' do
+    before do
+      allow_any_instance_of(ChefGit::BranchedDataBag).to receive(:chef_git_environment).and_return('notmaster')
+      allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return("0\t3")
+      allow_any_instance_of(Mixlib::ShellOut).to receive(:error?).and_return(false)
+      allow(Chef::DataBag).to receive(:load).and_return({})
+      Dir.stub(:chdir).and_yield
+    end
+
+    it 'returns the data bag name not prefixed by branch branch' do
+      expect(ChefGit::BranchedDataBag.bag_name('foo')).to eq('foo')
+    end
+  end
+
+  context 'branched data bag on the chef server' do
+    before do
+      allow_any_instance_of(ChefGit::BranchedDataBag).to receive(:chef_git_environment).and_return('notmaster')
+      allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return("0\t3")
+      allow_any_instance_of(Mixlib::ShellOut).to receive(:error?).and_return(false)
+      allow(Chef::DataBag).to receive(:load).and_return({'foo' => 'bar'})
+      Dir.stub(:chdir).and_yield
+    end
+
+    it 'returns the data bag name prefixed by branch branch' do
+      expect(ChefGit::BranchedDataBag.bag_name('foo')).to eq('notmaster__foo')
+    end
   end
 end
